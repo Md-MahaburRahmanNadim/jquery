5ebb683060ea20b215f93d0036fa645644ad08e2
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-07 03:52:29 +0100

    learning about submit event, mutiple event work on single selector, focus event

--- Source_Code/jquery_tutorial_7/js/forms.js
+++ Source_Code/jquery_tutorial_7/js/forms.js
@@ -1,13 +1,12 @@
-$("form").on("submit", function(){
-  var enteredWord = $("input#entered_word").val();
+$("form").on("submit", function () {
+  const enteredWord = $("input#entered_word").val(); // when user submit the button we catch the value of the input field and store it enteredWord variable
   alert(enteredWord);
 });
 
-$("button#loc_button").on("dblclick click", function(){
-  var enteredZip = $("input#entered_zip").val();
+$("button#loc_button").on("dblclick click", function () {
+  const enteredZip = $("input#entered_zip").val();
   $("input#entered_city_state").val(enteredZip + " is for NJ");
 });
-
-$("input#entered_zip").on("focus", function(){
-  $("h2#city_display").text("About to type:");
+$("#entered_zip").on("focus", () => {
+  $("#city_display").text("enter the type");
 });
