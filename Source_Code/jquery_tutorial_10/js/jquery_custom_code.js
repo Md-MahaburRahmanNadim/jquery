abfa4ebca2dd4639ae7768dad4eb284e0f1e8c81
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-09 18:08:53 +0100

    learning about attr,removeAttr, remove method and selector and the document ready fn

--- Source_Code/jquery_tutorial_10/js/jquery_custom_code.js
+++ Source_Code/jquery_tutorial_10/js/jquery_custom_code.js
@@ -1,5 +1,8 @@
-
-$(document).ready(function(){
-  
-
+// $(document).ready(function(){ old way to select the docoment. the version 3 is given below
+$(function () {
+  // this line of code give us opportunites to inject jquery code after the open body tag
+  // $("#steps li").css("color", "blue"); this select all li inside of the #steps id
+  // $("#steps > li").css("color", "red"); // direct child selector
+  // let's now learn about the filter selector in jquery which is called in css as a psodue selector
+  $("#steps li:first, #grand-child li:first").css("color", "blue");
 });
--- Source_Code/jquery_tutorial_9/js/jquery_custom_code.js
+++ Source_Code/jquery_tutorial_9/js/jquery_custom_code.js
@@ -1,21 +1,33 @@
 // $("p.instruct_title, div#closing_statement, h3").css("color", "green");
-$("p.instruct_title").add("div#closing_statement").add("h3").css("color", "green");
+$("p.instruct_title")
+  .add("div#closing_statement")
+  .add("h3")
+  .css("color", "green");
 
-$("li.important").on("mouseover", function(){
-    $(this).attr("id", "big");
-    $(this).on("mouseleave", function(){
-        $(this).attr("id", "small");
-    });
+$("li.important").on("mouseover", function () {
+  $(this).attr("id", "big");
+  $(this).on("mouseleave", function () {
+    $(this).attr("id", "small");
+  });
 });
 
-$("h1#title").on("click", function(){
-    $(this).attr("class", "pretty border");
+$("h1#title").on("click", function () {
+  $(this).attr("class", "pretty border");
 });
 
-$("img#the_img").on("click", function(){
-    $(this).attr({"width": "40%", "length": "40%"});
+$("img#the_img").on("click", function () {
+  // different ways of appling the multiple attribute in css by using attr method
+  /* 1st way by using multiple times the attr method
+  $(this).attr("width", "40%");
+  $(this).attr("height", "40%");*/
+  /**
+   by chaining the attr method one after another
+   $(this).attr('width','40%').attr('height','40%')
+   */
+  // final and smart way to add multiple attr at a time by using map data structure like an obj
+  $(this).attr({ width: "40%", length: "40%" });
 });
 
-$("img#the_img").on("dblclick", function(){
-    $(this).remove();
+$("img#the_img").on("dblclick", function () {
+  $(this).remove();
 });
