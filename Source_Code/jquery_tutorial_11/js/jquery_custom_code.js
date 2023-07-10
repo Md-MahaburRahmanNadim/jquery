1b8f5d6fc3ae87d95f476f7c1a945f40245d3b53
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-10 15:19:53 +0100

    selecting all div where those div dont have :header element

--- Source_Code/jquery_tutorial_11/js/jquery_custom_code.js
+++ Source_Code/jquery_tutorial_11/js/jquery_custom_code.js
@@ -1,5 +1,7 @@
-
-$(function(){
-    $(":header").css({"border" :"1px solid red"});
-
+$(function () {
+  // $(":header").css({"border" :"1px solid red"});
+  // assignment target those div that do not have header
+  // $("div:not(:header)").css({ 'border': "1px solid red" }); wrong answer
+  // $('div:has(:not(:header))').css({'border':'1px solid red'}); this work file
+  $('div:not(:has(:header))').css({'border':'1px solid red'});
 });
