dab8d9d5ddded6b809ccf5b6f8e0c721f48181e1
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-13 16:37:54 +0100

    assignment solution by instractor

--- Source_Code/jquery_tutorial_14/js/custom_jquery_code.js
+++ Source_Code/jquery_tutorial_14/js/custom_jquery_code.js
@@ -2,19 +2,17 @@ $(function () {
   /**
    *   I would like you to write the jquery code for this page to behave as I'm about to show you. When I click on these details button, it should show me the price of the particular book and the Author. And then it also highlights the background yellow. And also hide the button.
    */
-  $("#books_div button").mouseenter(function () {
-    $(this).click(function () {
-      $(this).parent().css("background-color", "yellow");
-      const getPrice = $(this).parent().data("price");
-      const getAuthor = $(this).parent().data("author");
-      $(this)
-        .parent()
-        .append($("<p>Price: " + getPrice + "</p>"));
-      $(this)
-        .parent()
-        .append($("<p>Author: " + getAuthor + "</p>"));
-      // $(this).off("click"); // This will remove the click event from the button
-      $(this).hide();
-    });
+  $("#books_div button").click(function () {
+    $(this).parent().css("background-color", "yellow");
+    const getPrice = $(this).closest(".book").data("price");
+    const getAuthor = $(this).closest(".book").data("author");
+    const details = `<p>Price: ${getPrice}</p><p>Author: ${getAuthor}</p>`;
+    // console.log(getPrice, getAuthor);
+    /**
+     * this is one way to doing this if we know that where we want to append the details
+     * $(this).closest(".book").append(details);
+    $(this).remove();
+     */
+    $(this).after(details).remove();
   });
 });
