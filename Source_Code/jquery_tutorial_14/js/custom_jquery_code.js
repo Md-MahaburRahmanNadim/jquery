2632471c518501ecafbd9baec3d932db03c73be0
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-13 06:20:59 +0100

    my solution for this assignment

--- Source_Code/jquery_tutorial_14/js/custom_jquery_code.js
+++ Source_Code/jquery_tutorial_14/js/custom_jquery_code.js
@@ -1,4 +1,20 @@
-
-$(function(){
-    
+$(function () {
+  /**
+   *   I would like you to write the jquery code for this page to behave as I'm about to show you. When I click on these details button, it should show me the price of the particular book and the Author. And then it also highlights the background yellow. And also hide the button.
+   */
+  $("#books_div button").mouseenter(function () {
+    $(this).click(function () {
+      $(this).parent().css("background-color", "yellow");
+      const getPrice = $(this).parent().data("price");
+      const getAuthor = $(this).parent().data("author");
+      $(this)
+        .parent()
+        .append($("<p>Price: " + getPrice + "</p>"));
+      $(this)
+        .parent()
+        .append($("<p>Author: " + getAuthor + "</p>"));
+      // $(this).off("click"); // This will remove the click event from the button
+      $(this).hide();
+    });
+  });
 });
