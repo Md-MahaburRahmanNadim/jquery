4de197d5c28f0196524c5758dd4a2fe3da7772cf
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2023-07-16 15:26:37 +0100

    complete the assignment

--- assignment code/jquery_assignment_e/js/custom_jquery_code.js
+++ assignment code/jquery_assignment_e/js/custom_jquery_code.js
@@ -1,4 +1,15 @@
-
-$(function(){
-    
+$(function () {
+  let inputText = "";
+  $("#btn_1").on("click", function () {
+    inputText = $("input").val();
+    $("#statement h3")
+      .text(`Course Entered: ${inputText}`)
+      .closest("#statement")
+      .css("background", "green");
+  });
+  $("#courses").on("click", "li", function (e) {
+    $(this).after($(`<li> ${inputText}</li>`));
+    $(this).next().css("background", "yellow");
+    e.stopPropagation();
+  });
 });
